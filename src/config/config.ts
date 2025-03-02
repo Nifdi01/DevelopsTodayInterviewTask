656f8d0e3f298c429f9ef90bfa2a6e03aea1f0ed
Nifdi Guliyev <nifdiguliyev@gmail.com>
2025-03-02 17:08:52 +0400

    Update dependencies and refactor Kafka configuration; remove unused test and Kafka files

--- src/config/config.ts
+++ src/config/config.ts
@@ -5,7 +5,7 @@ export const config = {
   port: process.env.PORT || 3000,
   llmApiKey: process.env.GEMINI_API_KEY || '',
   embeddingApiKey: process.env.GEMINI_EMBEDDING_ENDPOINT || '',
-  pineconeApiKey: process.env.PINECONE_API_KEY || '',
+  pineconeApiKey: process.env.PINECONE_KEY || '',
   pineconeIndexUrl: process.env.PINECONE_INDEX_URL || '',
   kafkaBroker: process.env.KAFKA_BROKER || '',
   kafkaUsername: process.env.KAFKA_USERNAME || '',
